1d41b37a50042f34fc8ced5c28a0841d955757a0
hossamelsahafy <hossamelsa7afy54@gmail.com>
2024-10-03 20:18:58 +0300

    files

--- Server/src/Controller/Admin/DoctorsController.js
+++ Server/src/Controller/Admin/DoctorsController.js
@@ -1,15 +1,15 @@
 const bcrypt = require("bcrypt");
 const { models } = require("mongoose");
-const DoctorModel = require("../models/Doctor");
-const departmentModel = require("../models/Departments");
+const DoctorModel = require("../../models/Doctor.js");
+const departmentModel = require("../../models/Departments");
 const { json } = require("express");
-const { updateDepartment } = require("./Admin/DepartmentController.js");
+const { updateDepartment } = require("../Admin/DepartmentController.js");
 const {
   generateAdvancedPassword,
   generateResetToken,
   generateToken,
-} = require("../Tools/GeneratePassword.js");
-const sendResetPasswordEmail = require("../Tools/SendEmailToDoctors.js");
+} = require("../../Tools/GeneratePassword.js");
+const sendResetPasswordEmail = require("../../Tools/SendEmailToDoctors.js");
 
 const emailRegex =
   /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/i;
