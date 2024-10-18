d31eb5b98d47fee6c25118949274e377013eee74
MoSalem149 <mohamed.salem.dev.official@gmail.com>
2024-10-18 05:04:01 +0300

    update patient home page

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,24 +1,24 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import SignUpPage from "./pages/SignUpPage";
-import LoginPage from "./pages/LoginPage";
-import ResetPasswordPage from "./pages/ResetPasswordPage";
-import ForgetPasswordPage from "./pages/ForgetPasswordPage";
-// import PatientHomePage from "./pages/PatientHomePage";
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// import SignUpPage from "./pages/SignUpPage";
+// import LoginPage from "./pages/LoginPage";
+// import ResetPasswordPage from "./pages/ResetPasswordPage";
+// import ForgetPasswordPage from "./pages/ForgetPasswordPage";
+import PatientHomePage from "./pages/PatientHomePage";
 import "./App.css";
 
 const App = () => {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<SignUpPage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/forget-password" element={<ForgetPasswordPage />} />
-        {/* <Route path="/reset-password" element={<ResetPasswordPage />} /> */}
-        <Route path="/reset-password/:token" element={<ResetPasswordPage />} />
-        <Route path="/signup" element={<SignUpPage />} />
-      </Routes>
-    </Router>
-    // <PatientHomePage />
+    // <Router>
+    //   <Routes>
+    //     <Route path="/" element={<SignUpPage />} />
+    //     <Route path="/login" element={<LoginPage />} />
+    //     <Route path="/forget-password" element={<ForgetPasswordPage />} />
+    //     {/* <Route path="/reset-password" element={<ResetPasswordPage />} /> */}
+    //     <Route path="/reset-password/:token" element={<ResetPasswordPage />} />
+    //     <Route path="/signup" element={<SignUpPage />} />
+    //   </Routes>
+    // </Router>
+    <PatientHomePage />
   );
 };
 
