9f28c0ab00b0850fa948474042fbacfe20ae872e
hossamelsahafy <hossamelsa7afy54@gmail.com>
2024-10-24 16:02:08 +0300

    files

--- Client/src/components/Header.jsx
+++ Client/src/components/Header.jsx
@@ -7,16 +7,39 @@ import injectionImage from "../assets/images/User-Profile-Page-img/injection-img
 import toolImage from "../assets/images/User-Profile-Page-img/tool-img.png";
 import searchIcon from "../assets/images/User-Profile-Page-img/search-icon.png";
 import avatarImage from "../assets/images/Patient-Home-Page-img/avatar1.png";
+import { useNavigate } from "react-router-dom";
+import { DoctorContext } from "./Doctor-details/DoctorContext";
+import { useContext } from "react";
+import { jwtDecode } from "jwt-decode";
 /* Import CSS */
 import "../styles/Header.css";
 
 function Header() {
+  const { doctors } = useContext(DoctorContext);
   /* State */
   const [menuActive, setMenuActive] = useState(false);
-
+  const userRole = localStorage.getItem("userRole");
+  const navigate = useNavigate();
   // Function to handle menu toggle
   const handleMenuToggle = () => {
     setMenuActive(!menuActive);
+  };
+  const token = localStorage.getItem("token");
+  const handleProfileClick = (e) => {
+    e.preventDefault();
+    if (userRole === "doctor") {
+      const userId = jwtDecode(token).id;
+      const matchedDoctor = doctors.find((doctor) => doctor.user === userId);
+      if (matchedDoctor) {
+        navigate("/doctor-profile", { state: { doctor: matchedDoctor } });
+      } else {
+        // console.error("Doctor not found");
+      }
+    } else if (userRole === "user") {
+      navigate("/user-profile");
+    } else {
+      // console.error("Unknown role, unable to navigate");
+    }
   };
   return (
     <>
@@ -25,7 +48,8 @@ function Header() {
         <nav>
           <div className="logo">
             <h1 className="logo-text">
-              <span className="header-span">C</span>are <span className="header-span">C</span>linics
+              <span className="header-span">C</span>are{" "}
+              <span className="header-span">C</span>linics
             </h1>
           </div>
           <ul className={`nav-links ${menuActive ? "active" : ""}`}>
@@ -50,9 +74,10 @@ function Header() {
             </button>
           </div>
           <div className="link">
-            <a href="#" className="profile-img">
+            <a href="#" className="profile-img" onClick={handleProfileClick}>
               <img src={avatarImage} alt="Profile" />
             </a>
+
             <a href="#" className="logout-btn">
               <FaSignOutAlt /> Sign Out
             </a>
@@ -67,7 +92,6 @@ function Header() {
         </nav>
       </header>
       {/* Header Images */}
-    
     </>
   );
 }
--- Client/src/pages/doctorProfile.jsx
+++ Client/src/pages/doctorProfile.jsx
@@ -18,9 +18,7 @@ function DoctorProfile() {
 
   useEffect(() => {
     if (currentUser && doctor) {
-      if (currentUser.id === doctor.id) {
-        console.log(currentUser.id);
-
+      if (currentUser.id === doctor.user) {
         setIsOwnerOfPof(true);
       }
     }
@@ -147,7 +145,7 @@ function DoctorProfile() {
 
         // API call to delete the account
         const doctorResponse = await fetch(
-          "http://localhost:5000/users/profile/delete",
+          `http://localhost:5000/users/profile/delete/${doctor._id}`,
           {
             method: "DELETE",
             headers: {
@@ -165,7 +163,7 @@ function DoctorProfile() {
         // Show success message with SweetAlert
         Swal.fire("Deleted!", "Your account has been deleted.", "success");
 
-        console.log(doctorResult); // Handle any additional logic here, e.g., logging out or redirecting
+        // console.log(doctorResult);
       }
     } catch (error) {
       console.error("Error deleting account:", error);
